209602de9557ac8bfeaff9fff5592ae0fef41e9e
dsb <dsbtek@gmail.com>
2025-06-27 23:05:57 +0100

    added health check route

--- backend/community/urls.py
+++ backend/community/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from .views import health_check
 
 # Swagger/OpenAPI schema configuration
 schema_view = get_schema_view(
@@ -31,6 +32,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui-root'),
+    path('api/health', health_check),
 ]
 
 if settings.DEBUG:
